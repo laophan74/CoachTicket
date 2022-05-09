d9b5bda3a11c159dfe3094631de7626dc143c459
Laophan74 <laophan74@gmail.com>
2022-05-10 00:28:29 +0700

    lay du lieu

--- app/src/main/java/com/uit/TripTicketSaler/SearchTrip.java
+++ app/src/main/java/com/uit/TripTicketSaler/SearchTrip.java
@@ -1,16 +1,52 @@
 package com.uit.TripTicketSaler;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
+import static android.content.ContentValues.TAG;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SearchTrip extends AppCompatActivity {
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
-
+    private DocumentReference noteRef = db.collection("Station").document("DWS769q3lo89ubfmxjnp");
+    private TextView textViewtest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_trip);
+        textViewtest = findViewById(R.id.textviewtest);
+
+        //Lấy data
+        noteRef.get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if(documentSnapshot.exists()){
+                            String name = documentSnapshot.getString("name");
+                            textViewtest.setText(name);
+                        }else {
+                            Toast.makeText(SearchTrip.this, "Document doesnt exist", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(SearchTrip.this, "Error", Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, e.toString());
+                    }
+                });
     }
 }
