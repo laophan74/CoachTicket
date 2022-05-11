a3bedec6c9266a941f6a808711e54fd25b8107e9
UIT.19521828 <19521828@gm.uit.edu.vn>
2022-05-11 14:52:00 +0700

    get data

---
+++ app/src/main/java/com/uit/TripTicketSaler/Model/Station.java
@@ -0,0 +1,24 @@
+package com.uit.TripTicketSaler.Model;
+
+public class Station {
+    private String address;
+    private String city;
+
+    public Station(){}
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
