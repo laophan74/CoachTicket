9db18764b3d8b9cc166857559e7767d343516ca4
Laophan74 <laophan74@gmail.com>
2022-05-27 04:37:18 +0700

    service + info

---
+++ app/src/main/java/com/uit/TripTicketSaler/PassengerInfomation.java
@@ -0,0 +1,14 @@
+package com.uit.TripTicketSaler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PassengerInfomation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passenger_infomation);
+    }
+}
