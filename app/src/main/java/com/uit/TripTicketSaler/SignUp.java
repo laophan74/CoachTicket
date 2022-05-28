ad5483f4d85c2a35e4e400173794d87766e7d9ed
Laophan74 <laophan74@gmail.com>
2022-05-28 22:16:08 +0700

    .

--- app/src/main/java/com/uit/TripTicketSaler/SignUp.java
+++ app/src/main/java/com/uit/TripTicketSaler/SignUp.java
@@ -1,14 +1,55 @@
 package com.uit.TripTicketSaler;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUp extends AppCompatActivity {
 
+    private TextView username;
+    private TextView password;
+    private TextView retypepassword;
+    private Button signup;
+
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        retypepassword = findViewById(R.id.retypepassword);
+        signup = findViewById(R.id.signupbtn);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.createUserWithEmailAndPassword(username.getText().toString(),password.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Toast.makeText(SignUp.this, "Thanh Cong", Toast.LENGTH_SHORT).show();
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Toast.makeText(SignUp.this, "Tai khoan"+user.getEmail(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
