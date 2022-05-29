5f212eca5e00cc822bc4234cd40e794487d4bc2a
Laophan74 <laophan74@gmail.com>
2022-05-30 02:12:53 +0700

    +

---
+++ app/src/main/java/com/uit/TripTicketSaler/TicketInfor.java
@@ -0,0 +1,14 @@
+package com.uit.TripTicketSaler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TicketInfor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ticket_infor);
+    }
+}
